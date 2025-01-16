1507edcf473bb8d353d3570220e8c9eaa353d96f
hvgupta <hvgupta@outlook.in>
2025-01-16 22:51:18 +0800

    changed function names

--- Relationship/GraphDB_Creator.py
+++ Relationship/GraphDB_Creator.py
@@ -7,18 +7,18 @@ from Utils import *
 from Database import Data_Access_Facade
 from time import sleep
 
-def load_output(file:str)->dict:
+def __load_output(file:str)->dict:
     with open(file, "r") as outputFile:
         output = json.load(outputFile)
     return output
 
-def getWebsiteInfo(inputFile:str):
-    output = load_output(inputFile)
+def __getWebsiteInfo(inputFile:str):
+    output = __load_output(inputFile)
     for key in output:
         yield key, {"text":removeStopWordsandStem(output[key]["text"]), "summary":removeStopWordsandStem(output[key]["summary"])}
 
 def createGraphDB(inputFile:str):
-    for key, value in getWebsiteInfo(inputFile):
+    for key, value in __getWebsiteInfo(inputFile):
         Data_Access_Facade.createNode("Website",{"name":getNameFromUrl(key), "url":key})
         for text_key, text_val in value.items():
             stemmedText = removeStopWordsandStem(text_val)
